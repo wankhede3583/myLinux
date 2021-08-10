00d77b2947ce55f9bf34b3a9b10a36fe3ab1aed4
wankhede3583 <wankhede3583@gmail.com>
2021-08-10 23:13:04 +0530

    add all files

---
+++ scripts/pk_script/3readuserinupt.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# this is script for read user input
+# -p for show input in single line
+ # -s for hide password form user
+read -p "Enter user name : " user_var 
+read -sp "Enter Password : " pass_var
+echo "your username is : " $user_var
+echo "your password is : " $pass_var
